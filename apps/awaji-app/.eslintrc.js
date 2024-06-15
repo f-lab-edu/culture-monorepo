a92210cab7979d3e96fa2ff5f8d5cc57f4d835d0
Minji Kang <71913557+awaji0829@users.noreply.github.com>
2024-06-15 15:24:10 +0900

    apps 폴더 git 제거

---
+++ apps/awaji-app/.eslintrc.js
@@ -0,0 +1,9 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: [require.resolve("@repo/eslint/index.js")],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+};
