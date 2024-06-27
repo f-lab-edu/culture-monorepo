c157eb2be3da70e38d410ca5e4fd03ea35b169e5
Minji Kang <71913557+awaji0829@users.noreply.github.com>
2024-06-27 21:57:05 +0900

    chore : 스토리북 초기세팅

---
+++ apps/awaji-app/.storybook/main.ts
@@ -0,0 +1,44 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import { join, dirname } from "path";
+const path = require("path");
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {},
+  },
+  webpackFinal: async (config) => {
+    if (!config.module) {
+      config.module = { rules: [] };
+    }
+
+    // config.module.rules가 undefined일 경우 초기화
+    if (!config.module.rules) {
+      config.module.rules = [];
+    }
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+
+    return config;
+  },
+};
+export default config;
